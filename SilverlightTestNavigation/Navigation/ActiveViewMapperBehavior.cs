9f187071e2d53b1ee05e789db24eecadfb6febac
adimkov <ddt.sterh@gmail.com>
2011-08-28 21:07:08 +0300

    Added menthos in INavigationBar

--- SilverlightTestNavigation/Navigation/ActiveViewMapperBehavior.cs
+++ SilverlightTestNavigation/Navigation/ActiveViewMapperBehavior.cs
@@ -3,17 +3,17 @@
     using System.Collections.Specialized;
 
     /// <summary>
-    /// Mapping in the browser navigation bar an active view in a region
+    /// Mapping in the browser navigation bar an active view in a region.
     /// </summary>
     public class ActiveViewMapperBehavior : RegionBehavior
     {
         /// <summary>
-        /// The key of behavior
+        /// The key of behavior.
         /// </summary>
         public const string BehaviorKey = "ActiveViewMapperBehavior";
 
         /// <summary>
-        /// Holds reference to navigation bar
+        /// Holds reference to navigation bar.
         /// </summary>
         private INavigationBar _navigatioBar;
 
@@ -21,7 +21,7 @@
         /// Initializes a new instance of the <see cref="ActiveViewMapperBehavior"/> class. 
         /// </summary>
         /// <param name="navigatioBar">
-        /// Navigation bar
+        /// Navigation bar.
         /// </param>
         public ActiveViewMapperBehavior(INavigationBar navigatioBar)
         {
@@ -29,12 +29,12 @@
         }
 
         /// <summary>
-        /// Gets or sets a value indicating whether enabled or disabled mapping of active view in navigation bar
+        /// Gets or sets a value indicating whether enabled or disabled mapping of active view in navigation bar.
         /// </summary>
         public bool IsEnabled { get; set; }
 
         /// <summary>
-        /// Attaches behavior to region
+        /// Attaches behavior to region.
         /// </summary>
         protected override void OnAttach()
         {
@@ -42,21 +42,38 @@
         }
 
         /// <summary>
-        /// Handles event of changing active view collection
+        /// Handles event of changing active view collection.
         /// </summary>
-        /// <param name="sender">The region</param>
-        /// <param name="args">The arguments</param>
+        /// <param name="sender">The region.</param>
+        /// <param name="args">The arguments.</param>
         private void ActiveViewsOnCollectionChanged(object sender, NotifyCollectionChangedEventArgs args)
         {
+            string newViewName = string.Empty;
+            string oldViewName = string.Empty;
+            
+            if (args.NewItems.Count > 0)
+            {
+                 newViewName = args.NewItems[0].GetType().Name;
+            }
+
+            if (args.OldItems.Count > 0)
+            {
+                oldViewName = args.OldItems[0].GetType().Name;
+            }
+
             switch (args.Action)
             {
                 case NotifyCollectionChangedAction.Add:
+                    _navigatioBar.AddFragment(newViewName);
                     break;
                 case NotifyCollectionChangedAction.Remove:
+                    _navigatioBar.RemoveFragment(oldViewName);
                     break;
                 case NotifyCollectionChangedAction.Replace:
+                    _navigatioBar.ReplaceFragment(oldViewName, newViewName);
                     break;
                 case NotifyCollectionChangedAction.Reset:
+                    _navigatioBar.ClearFragments();
                     break;
             }
         }
--- SilverlightTestNavigation/Navigation/INavigationBar.cs
+++ SilverlightTestNavigation/Navigation/INavigationBar.cs
@@ -1,10 +1,32 @@
 ﻿namespace Microsoft.Practices.Prism.Regions
 {
     /// <summary>
-    /// Contract to navigation bar
+    /// Contract to navigation bar.
     /// </summary>
     public interface INavigationBar
     {
-             
+        /// <summary>
+        /// Adds fragment to url.
+        /// </summary>
+        /// <param name="fragment">Fragment to add.</param>
+        void AddFragment(string fragment);
+
+        /// <summary>
+        /// Remove fragment from url.
+        /// </summary>
+        /// <param name="fragment">Fragment to remove.</param>
+        void RemoveFragment(string fragment);
+
+        /// <summary>
+        /// Replace old fragment by new one.
+        /// </summary>
+        /// <param name="oldFragment">Fragment that should by replaced.</param>
+        /// <param name="newFragment">New fragment.</param>
+        void ReplaceFragment(string oldFragment, string newFragment);
+
+        /// <summary>
+        /// Clear fragments in url.
+        /// </summary>
+        void ClearFragments();
     }
 }
