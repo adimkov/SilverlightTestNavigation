01c60a7f21d8e67918adda8680db7ed23bc2ecbc
Anton Dimkov <ddt.sterh@gmail.com>
2011-09-01 23:14:18 +0300

    Added common project

---
+++ SilverlightTestNavigation.Common/Modularity/ModuleBase.cs
@@ -0,0 +1,41 @@
+﻿namespace SilverlightTestNavigation.Common.Modularity
+{
+    using Autofac;
+
+    using Microsoft.Practices.Prism.Modularity;
+    using Microsoft.Practices.Prism.Regions;
+
+    /// <summary>
+    /// Base class for modules
+    /// </summary>
+    /// <remarks></remarks>
+    public abstract class ModuleBase : IModule
+    {
+        /// <summary>
+        /// Gets the IoC container.
+        /// </summary>
+        /// <remarks>The container can be completed by derived module.</remarks>
+        protected IContainer Container { get; private set; }
+
+        /// <summary>
+        /// Gets the region manager.
+        /// </summary>
+        protected RegionManager RegionManager { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ModuleBase"/> class.
+        /// </summary>
+        /// <param name="container">The container.</param>
+        /// <param name="regionManager">The region manager.</param>
+        public ModuleBase(IContainer container, RegionManager regionManager)
+        {
+            Container = container;
+            RegionManager = regionManager;
+        }
+
+        /// <summary>
+        /// Notifies the module that it has be initialized.
+        /// </summary>
+        public abstract void Initialize();
+    }
+}
